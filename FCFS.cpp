e593324437b50185f118951d3dc1d6266711993d
Mayank Joshi <mayankjan2002@gmail.com>
2021-10-10 20:36:48 +0530

    .

---
+++ FCFS.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+using namespace std;
+class Process{
+	public:
+	int pid;
+	int arrivalTime;
+	int burstTime;
+	
+	void input(int,int,int);
+};
+void Process::input(int a,int b,int c)
+{
+	pid = a;
+	arrivalTime = b;
+	burstTime =c;
+}
+bool SORT(Process a,Process b)
+{
+	if(a.arrivalTime!=b.arrivalTime)
+		return a.arrivalTime<b.arrivalTime;
+	return a.pid<b.pid;
+}
+int main()
+{
+	int pid,arrivalTime,burstTime;
+	int nop;
+	cin>>nop;
+	Process P[nop];
+	for(int i = 0;i<nop;i++)
+	{
+		cin>>pid>>arrivalTime>>burstTime;
+		P[i].input(pid,arrivalTime,burstTime);
+	}
+	sort(P,P+nop,SORT);
+	for(int i = 0;i<nop;i++)
+	{
+		cout<<endl<<P[i].pid<<" "<<P[i].arrivalTime<<" "<<P[i].burstTime;
+		// P[i].input(pid,arrivalTime,burstTime);
+	}
+	return 0;
+}
